285c9af60466ac43c73a0da48f16c01032dcded5
Brax Wong <ghostenjob@gmail.com>
2023-10-13 16:29:34 +0100

    Feature: Installing python modules before launching the game.

--- BashFiles/start.sh
+++ BashFiles/start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+pip3 install opencv-python==4.6.0.66
+pip3 install mediapipe
+pip3 install numpy
+pip3 install python-osc
 ./../DanceForMeExe/MacDanceForMe.app/Contents/MacOS/DanceForMeUnity &
 python3 ../DanceForMeUnity/Assets/Scripts/PythonScripts/Starting.py 
